602ad5789e09bb48965b0f58649b491f4716d91e
PhanindharBodla <springbull2020@mail.com>
2013-01-31 19:38:58 +0530

    Bean Processors Done..!

--- SpringBeanPostProcessors/src/com/jda/InitDemoSpring.java
+++ SpringBeanPostProcessors/src/com/jda/InitDemoSpring.java
@@ -5,3 +5,24 @@ package com.jda;
  *
  * @author Phanindhar Bodla
  */
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.BeansException;
+
+public class InitDemoSpring implements BeanPostProcessor {
+ 
+    @Override
+   public Object postProcessBeforeInitialization(Object bean,
+                 String beanName) throws BeansException {
+      System.out.println("BeforeInitialization : " + beanName);
+      return bean;  // you can return any other object as well
+   }
+
+    @Override
+   public Object postProcessAfterInitialization(Object bean,
+                 String beanName) throws BeansException {
+      System.out.println("AfterInitialization : " + beanName);
+      return bean;  // you can return any other object as well
+   }
+
+}
