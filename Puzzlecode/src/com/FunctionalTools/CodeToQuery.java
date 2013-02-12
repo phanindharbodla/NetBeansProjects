9c461d436401901bf686fa2378823244859c7983
PhanindharBodla <springbull2020@mail.com>
2013-02-12 19:24:08 +0530

    Some Utilities done.!!

---
+++ Puzzlecode/src/com/FunctionalTools/CodeToQuery.java
@@ -0,0 +1,55 @@
+/*
+ * Give the code of load query string in code.txt get 
+ * query at query .txt
+ */
+package com.FunctionalTools;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author Phanindhar Bodla
+ */
+public class CodeToQuery {
+
+    public static void main(String args[]) {
+        try {
+            File inputFile = new File("C:\\Users\\j1013563\\Documents\\NetBeansProjects\\Puzzlecode\\src\\com\\FunctionalTools\\loadquery.txt");//Give your input load query ..
+            BufferedReader br = new BufferedReader(new FileReader(inputFile));
+            String strLine;
+            BufferedWriter outputFile = new BufferedWriter(new FileWriter("C:\\Users\\j1013563\\Documents\\NetBeansProjects\\Puzzlecode\\src\\com\\FunctionalTools\\query.txt"));//the query .text.txt is your output file
+            while ((strLine = br.readLine()) != null) {
+                strLine = process(strLine);
+                outputFile.write(strLine);
+                outputFile.newLine();
+            }
+            outputFile.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static String process(String strLine) {
+        String line = "";
+        int j = strLine.length();
+        try {
+            if (j != 0) {
+                for (int i = 0; i < j; i++) {
+                    if (strLine.charAt(i) == '"') {
+                        i++;
+                        while (strLine.charAt(i) != '"') {
+                            line = line + strLine.charAt(i);
+                            i++;
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+        return line;
+    }
+}
--- Puzzlecode/src/com/Patterns/AllowableChars.java
+++ Puzzlecode/src/com/Patterns/AllowableChars.java
@@ -4,26 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AllowableChars {
-  public static void main(String args[]) {
-    String regex = "[^-.0-9]";
+  public static void main(String args[]) { // removes numbers appended with # following characters..!!
+    String regex = "[^[#&[0-9]*]]";
     Pattern pattern = Pattern.compile(regex);
-    regex = "[^#&&[0-9]]";
-    Pattern pattern1 = Pattern.compile(regex);
-    String candidate = "-10.wer45";
+    String candidate = "asd5#123: ads";
     Matcher matcher = pattern.matcher(candidate);
     String temp=matcher.replaceAll("");
     System.out.println("ORIGINAL: " + candidate);
     System.out.println("REPLACEMENT: " + temp);
-    String candidate1 = "man#10 "+"a";
-    regex = "[#]";
-    Pattern pattern2 = Pattern.compile(regex);
-    matcher = pattern1.matcher(candidate1);
-    temp=matcher.replaceAll("");
-    System.out.println("ORIGINAL: " + candidate1);
-    System.out.println("REPLACEMENT: " + temp);
-    matcher = pattern2.matcher(temp);
-    String temp1=matcher.replaceAll("");
-    System.out.println("ORIGINAL: " + temp);
-    System.out.println("REPLACEMENT: " + temp1);
   }
 }
