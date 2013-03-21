bd366207f557daaeb86300f2ea89da8c86067a85
PhanindharBodla <springbull2020@mail.com>
2013-03-21 16:20:16 +0530

    Ran FT Utils..!!

---
+++ 124/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: j1013563
+ *
+ * Created on March 21, 2013, 2:37 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    std::cout<<"mhan"<<endl;
+    return 0;
+}
+
