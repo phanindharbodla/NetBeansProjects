a3f693817c4bcacb06c75934074306cef6b41409
PhanindharBodla <springbull2020@mail.com>
2013-04-18 18:13:58 +0530

    Made a GUI Utility For Making Jar with the selected class files from GUI .

--- Puzzlecode/src/com/test/Crazycalls.java
+++ Puzzlecode/src/com/test/Crazycalls.java
@@ -10,11 +10,13 @@ public class Crazycalls {
     public static void main(String args[]) throws IOException
     {
     boolean b=true;
+    double a=1234.81268477126781582,c=1234.8126847126781582;
     System.out.println("Lets do some crazy things ..!! Like Opening Eclipse here..!!");
-    String myShellScript="C:\\juno\\eclipse.exe -data  D:\\workspaces\\eclipse\\TPO_7720  -vmargs -Xms1024m -Xmx1024m -XX:MaxPermSize=128m";
-    Runtime.getRuntime().exec(myShellScript);
-    b=!b;
-    System.out.println(" "+(b));
+    //String myShellScript="C:\\juno\\eclipse.exe -data  D:\\workspaces\\eclipse\\TPO_7720  -vmargs -Xms1024m -Xmx1024m -XX:MaxPermSize=128m";
+    //Runtime.getRuntime().exec(myShellScript);
+    //b=!b;
+    //System.out.println(" "+(b));
+    //for()
     
 }
 
--- Puzzlecode/src/com/test/GuavaTest.java
+++ Puzzlecode/src/com/test/GuavaTest.java
@@ -7,6 +7,8 @@ package com.test;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -16,11 +18,36 @@ public class GuavaTest {
     public static void main(String args[])
     {
         BiMap<Integer, String> bimap = HashBiMap.create();
+        
+        Map <Object ,C > B =new HashMap<Object, C>();
+        Map <Object ,B > A =new HashMap<Object, B>();
+        Map <A ,B > Map =new HashMap<A, B>();
         //Map i=new HashMap();
         bimap.put(121, "string");
         bimap.put(123, "strn2g");
         bimap.put(123, "rng123");
         
+    }
+
+    private static class C {
+
+        public C() {
+            Map <Object ,String> ObjectC =new HashMap<Object, String>();
+        }
+    }
+
+    private static class B {
+
+        public B() {
+            Map <Object ,C> ObjectB =new HashMap<Object, C>();
+        }
+    }
+
+    private static class A {
+
+        public A() {
+            Map <Object ,B> ObjectA =new HashMap<Object, B>();
+        }
     }
 
 }
